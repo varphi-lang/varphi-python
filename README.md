ec6a0ec084ba2568eeb963910b188f1d7beaaccf
Hassan El-Sheikha <hmelsheikha@gmail.com>
2025-08-30 20:53:18 -0400

    feat: Add Varphi-Python compiler
    Just pure Python target, no debug info (yet)

---
+++ README.md
@@ -0,0 +1,1 @@
+# Varphi-Python Compiler
---
+++ src/varphi_python/cli.py
@@ -0,0 +1,28 @@
+
+import typer
+from pathlib import Path
+
+
+def varphi_python(
+    input_file: Path = typer.Argument(
+        ...,
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        resolve_path=True,
+        help="Path to input Varphi source file"
+    )
+):
+    from varphi_devkit import compile_varphi
+    from .core import VarphiToPythonCompiler
+    typer.echo(compile_varphi(
+        input_file.read_text(encoding="utf-8"),
+        VarphiToPythonCompiler()
+    ))
+
+def main():
+    typer.run(varphi_python)
+
+if __name__ == "__main__":
+    main()
